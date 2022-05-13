9ec506237a82e651dbd47dc277a7c7afc367e642
Mitchblahman <mitchell.aiken@yahoo.com>
2022-05-12 19:03:57 -0700

    Pictures
    created pictures of the folds
    python can calculate up to 25 folds, but can only graph up to 24 with enough resolution to make out the lines

--- dragoncurve.py
+++ dragoncurve.py
@@ -5,22 +5,23 @@ import matplotlib.pyplot as plt
 
 print("How many times would you like to fold the paper?")
 f = int(input())
-
-
-x = [0, 1]          #zero fold coordinates
+   
+x = [0, 1]            #zero fold coordinates
 y = [0, 0]
 
-for t in range (f): #t starts at 0, this runs as many times as there are folds
-#it must start with the current final coord and work back through for each doubling
-#might be better to build each new segment and append the whole thing, might not make a difference?
+for t in range (f):   #runs as many times as there are folds
     size = (2**t)     #size of current list
   
     for xt in range (2**t):
-        x.append(x[-1] + (y[size - (xt + 1)] - y[size - xt])) #x always needs to be looking at the end of the list
+        x.append(x[-1] + (y[size - (xt + 1)] - y[size - xt] )) #x bases its new positions on y's going back to the origin
     
     for yt in range (2**t):
-        y.append(y[-1] + (x[size - yt] - x[size - (yt + 1)]))
+        y.append(y[-1] - (x[size - (yt + 1)] - x[size - yt] )) #y bases its new positions on x's going back to the origin
 
+    print(str(t + 1) + '/' + str(f))     #how far along the program is
 
-fig, dragon = plt.subplots() #the graphs will need to be formatted square based on the size of the curve
-dragon.plot(x,y);
+fig, ax = plt.subplots() #the graphs will need to be formatted square based on the size of the curve
+ax.set_aspect('equal')    #sets square aspect ratio
+ax.axis('off')
+ax.plot(x, y, linewidth = .05);   #line width needs to be adjusted based on folds
+#plt.savefig(str(f) + ' folds', dpi = 2400)     #saves a png with the number of folds in its name
